1a0efe22a2c30078ac0aba78f5d8260034c4b69a
nfebrian13 <nanaz.febrian@gmail.com>
2023-03-25 16:03:56 +0700

    added data dummy

--- springboot-mybatis-jasper/src/main/resources/data.sql
+++ springboot-mybatis-jasper/src/main/resources/data.sql
@@ -3,3 +3,18 @@ values(10001,'Nana', 'E1234567');
 
 insert into student
 values(10002,'Alvaro', 'A1234568');
+
+insert into employee
+values(1001,'Alvin', 'Product Manager','25000000','12/02/2021');
+
+insert into employee
+values(1002,'Arshinta', 'System Architecture','20000000','11/03/2020');
+
+insert into employee
+values(1003,'Aldi', 'General Manager','28000000','01/02/2001');
+
+insert into employee
+values(1004,'Alma', 'Tribe Leader','19000000','14/02/2020');
+
+insert into employee
+values(1005,'Asep', 'Senior Developer','28000000','12/06/2021');
--- springboot-mybatis-jasper/src/main/resources/schema.sql
+++ springboot-mybatis-jasper/src/main/resources/schema.sql
@@ -5,3 +5,13 @@ create table student
    passport_number varchar(255) not null,
    primary key(id)
 );
+
+create table employee
+(
+   id integer not null,
+   name varchar(255) not null,
+   designation varchar(255) not null,
+   salary numeric(20,2) not null,
+   doj varchar(255) not null,
+   primary key(id)
+);
